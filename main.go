9339ff541ba2f1278fdd638e3966b04c0da17430
izra4 <aryaizra2@gmail.com>
2023-07-06 18:04:28 +0700

    test cors 2

--- main.go
+++ main.go
@@ -57,7 +57,19 @@ func main() {
 	paymentHandler := PaymentHandler(db)
 
 	r := gin.Default()
-	initializers.Cors(r)
+	r.Use(func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Max-Age", "86400")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		if c.Request.Method == "OPTIONS" {
+			c.Writer.Header().Set("Content-Type", "application/json")
+			c.AbortWithStatus(204)
+		} else {
+			c.Next()
+		}
+	})
 	r.GET("/test", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "succes",
